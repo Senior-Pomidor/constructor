bf17126721fde8ba0998cd82df4046607e4b34af
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-10-06 16:50:19 +0300

    Генерация блоков Html из JS модели

--- index.js
+++ index.js
@@ -20,3 +20,39 @@ const model = [
 		]
 	}
 ]
+
+// переменные DOM-элементы будем именовать с $ чтобы отличать от обычных переменных
+const $site = document.querySelector('#site')
+
+model.forEach(block => {
+	// console.log(block)
+	let html = '' // формируемый html
+
+	if (block.type === 'title') {
+		// html для Заголовка
+		html = `
+			<div class="row">
+				<div class="col-sm">
+					<h1>${block.value}</h1>
+				</div>
+			</div>
+		`
+	} else if (block.type === 'text') {
+		// html для Параграфа
+		html = `
+			<div class="row">
+				<div class="col-sm">
+					<p>
+						${block.value}
+					</p>
+				</div>
+			</div>
+		`
+	} else if (block.type === 'columns') {
+		
+	}
+
+	// вставка HTML в определённое место
+	// $site.insertAdjacentHTML('куда', что)
+	$site.insertAdjacentHTML('beforeend', html)
+})
